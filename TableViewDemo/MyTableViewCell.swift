bdf40f44db1691701389a994896afd752489b1ff
Swift Goose <swiftgoose19@gmail.com>
2023-02-23 23:46:51 -0500

    first commit

---
+++ TableViewDemo/MyTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//
+//
+// Created by Swift Goose.
+// Copyright (c) 2022 Swift Goose. All rights reserved.
+//
+// YouTube: https://www.youtube.com/channel/UCeHYBwcVqOoyyNHiAf3ZrLg
+//
+
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var customImageView: UIImageView!
+    @IBOutlet weak var customLabel: UILabel!
+    
+    static let identifier = "MyTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MyTableViewCell", bundle: nil)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TableViewDemo/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//
+//
+// Created by Swift Goose.
+// Copyright (c) 2022 Swift Goose. All rights reserved.
+//
+// YouTube: https://www.youtube.com/channel/UCeHYBwcVqOoyyNHiAf3ZrLg
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var strings: [String] = ["Alpha", "Beta", "Unlimited", "Revised"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        let vc = storyboard.instantiateViewController(withIdentifier: "testVC")
+        
+        vc.navigationItem.title = strings[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.identifier, for: indexPath) as! MyTableViewCell
+        cell.customImageView.image = UIImage(systemName: "square.fill")
+        
+        switch indexPath.row {
+        case 0:
+            cell.customImageView.tintColor = .orange
+        case 1:
+            cell.customImageView.tintColor = .cyan
+        case 2:
+            cell.customImageView.tintColor = .green
+        default:
+            cell.customImageView.tintColor = .black
+        }
+        
+        cell.customLabel.text = strings[indexPath.row]
+        return cell
+    }
+    
+    
+}
